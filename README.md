dccd6d213d12b0235e568a723781d42ef0c2babb
David Lee <david.lee@datadoghq.com>
2022-03-14 17:01:12 -0400

    Tightened template lambda role permissions, add prod publish code to publish script

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Serverless Sample App
 
-[![Launch Stack](https://s3.amazonaws.com/cloudformation-examples/cloudformation-launch-stack.png)](https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?stackName=datadog-serverless-sample-app&templateURL=https://david-cloudformation-template-test.s3.amazonaws.com/aws/sample-app-staging/latest.yaml)
+[![Launch Stack](https://s3.amazonaws.com/cloudformation-examples/cloudformation-launch-stack.png)](https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?stackName=datadog-serverless-sample-app&templateURL=https://datadog-cloudformation-template-sandbox.s3.amazonaws.com/aws/sample-app-staging/latest.yaml)
 
 Steps for trying out Datadog:
 
-1. Click "Launch Stack" above, provide your Datadog API key in the parameters section.
-1. Ensure you are on a supported AWS region (see above), and click "Create Stack".
-1. Once the stack has finished creating, open the "Outputs" tab in the stack information view.
-1. Invoke your stack 3-4 times by visiting the "ApiGatewayInvokeURL" url given in the "Outputs" tab.
+1. Click "Launch Stack" above.
+1. Enter Datadog API Key and the Datadog Site you are registered with and click `Create Stack`.
+1. Once the stack has finished creating, open the `Outputs` tab in the stack information view.
+1. Invoke your stack 3-4 times by visiting the `ApiGatewayInvokeURL` url given in the `Outputs` tab.
 1. Visit Datadog's serverless function view (such as https://app.datadoghq.com/functions for those registered on the US1 site) and search for the "datadog-serverless-sample-app" tag
 
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,26 +1,128 @@
 #!/bin/bash
 
+# Usage: ./publish.sh <Desired Version> <Account [sandbox|prod]>
+# When publishing to sandbox, the template version number is NOT updated, and no github release is created!
+
 set -e
 
 cd "$(dirname "$0")"/..
 
-BUCKET="david-cloudformation-template-test"
+CURRENT_VERSION=$(grep -o 'Version: \d\+\.\d\+\.\d\+' template.yaml | cut -d' ' -f2)
+
+# Read the desired version
+if [ -z "$1" ]; then
+    echo "ERROR: You must specify a desired version number"
+    exit 1
+elif [[ ! $1 =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
+    echo "ERROR: You must use a semantic version (e.g. 3.1.4)"
+    exit 1
+else
+    SAMPLE_APP_VERSION=$1
+fi
+
+# Check account parameter
+VALID_ACCOUNTS=("sandbox" "prod")
+if [ -z "$2" ]; then
+    echo "ERROR: You must pass an account parameter. Please choose sandbox or prod."
+    exit 1
+fi
+if [[ ! "${VALID_ACCOUNTS[@]}" =~ $2 ]]; then
+    echo "ERROR: The account parameter was invalid. Please choose sandbox or prod."
+    exit 1
+fi
+
+ACCOUNT="${2}"
+
+if [ "$ACCOUNT" = "sandbox" ]; then
+    BUCKET="datadog-cloudformation-template-sandbox"
+fi
+if [ "$ACCOUNT" = "prod" ]; then
+    BUCKET="datadog-cloudformation-template"
+fi
 
 function aws-login() {
     cfg=( "$@" )
     shift
-    aws-vault exec sandbox-account-admin --  ${cfg[@]}
+    if [ "$ACCOUNT" = "prod" ] ; then
+        aws-vault exec prod-engineering --  ${cfg[@]}
+    else
+        aws-vault exec sandbox-account-admin --  ${cfg[@]}
+    fi
 }
 
-echo "Inject code inline into cloudformation template"
+echo "Injecting lambda code into CloudFormation template"
 rm -rf dist
 mkdir dist
 
 awk -v STRING_TO_REPLACE="INJECT_ENTRY_FUNCTION_CODE" -f scripts/inject_inline_code.awk handler.py template.yaml > dist/template.yaml
 awk -v STRING_TO_REPLACE="INJECT_SQS_CONSUMER_CODE" -f scripts/inject_inline_code.awk handler.js dist/template.yaml > tmp && mv tmp dist/template.yaml
 
-echo "Validating template.yaml"
+# Validate the template
+echo "Validating template.yaml..."
 aws-login aws cloudformation validate-template --template-body file://dist/template.yaml
 
-echo "Uploading template and code zips to s3"
-aws-login aws s3 cp dist/template.yaml s3://${BUCKET}/aws/sample-app-staging/latest.yaml
+echo "Uploading the CloudFormation Template"
+if [ "$ACCOUNT" = "prod" ] ; then
+    exit 1
+     # Confirm to proceed
+    echo
+    read -p "About to bump the version from ${CURRENT_VERSION} to ${SAMPLE_APP_VERSION}, create a release of aws-dd-forwarder-${FORWARDER_VERSION} on GitHub, upload the template.yaml to s3://${BUCKET}/aws/sample-app/${SAMPLE_APP_VERSION}.yaml. Continue (y/n)?" CONT
+    if [ "$CONT" != "y" ]; then
+        echo "Exiting..."
+        exit 1
+    fi
+
+    # Get the latest code
+    git pull origin master
+
+    # Bump version number in settings.py and template.yml
+    echo "Bumping the version number to ${SAMPLE_APP_VERSION}..."
+    perl -pi -e "s/Version: [0-9\.]+/Version: ${SAMPLE_APP_VERSION}/g" template.yaml
+    perl -pi -e "s/Version: [0-9\.]+/Version: ${SAMPLE_APP_VERSION}/g" dist/template.yaml
+
+    # Commit version number changes to git
+    echo "Committing version number change..."
+    git add template.yaml
+    git commit -m "Bump version from ${CURRENT_VERSION} to ${SAMPLE_APP_VERSION}"
+    git push origin master
+
+    # Create a GitHub release
+    echo
+    echo "datadog-serverless-sample-app-${SAMPLE_APP_VERSION} to GitHub..."
+    go get github.com/github/hub
+
+    # "-a $BUNDLE_PATH" to include assets in github release
+    hub release create -m "datadog-serverless-sample-app-${SAMPLE_APP_VERSION}" datadog-serverless-sample-app-${SAMPLE_APP_VERSION}
+
+    # Make sure we are on the master branch
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    if [ $BRANCH != "master" ]; then
+        echo "ERROR: Not on the master branch, aborting."
+        exit 1
+    fi
+    aws-login aws s3 cp dist/template.yaml s3://${BUCKET}/aws/sample-app/${SAMPLE_APP_VERSION}.yaml \
+        --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws-login aws s3 cp dist/template.yaml s3://${BUCKET}/aws/sample-app/latest.yaml \
+        --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    TEMPLATE_URL="https://${BUCKET}.s3.amazonaws.com/aws/sample-app/latest.yaml"
+else
+    aws-login aws s3 cp dist/template.yaml s3://${BUCKET}/aws/sample-app-staging/${SAMPLE_APP_VERSION}.yaml
+    aws-login aws s3 cp dist/template.yaml s3://${BUCKET}/aws/sample-app-staging/latest.yaml
+    TEMPLATE_URL="https://${BUCKET}.s3.amazonaws.com/aws/sample-app-staging/latest.yaml"
+    echo "CURRENT_VERSION: $CURRENT_VERSION"
+    echo "SAMPLE_APP_VERSION: $SAMPLE_APP_VERSION"
+    echo "ACCOUNT: $ACCOUNT"
+    echo "TEMPLATE_URL: $TEMPLATE_URL"
+    echo "BUCKET: $BUCKET"
+fi
+
+echo "Done uploading the CloudFormation template!"
+echo
+echo "Here is the CloudFormation quick launch URL:"
+echo "https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?stackName=datadog-serverless-sample-app&templateURL=${TEMPLATE_URL}"
+echo
+echo "Serverless Sample App release process complete!"
+
+if [ "$ACCOUNT" = "prod" ] ; then
+    echo "Don't forget to add release notes in GitHub!"
+fi
