1ade9f95d1649371997de282ce06499fe17fd7c1
james.eastham <james.eastham@datadoghq.com>
2024-10-04 14:05:54 +0100

    Update Java API SSM parameter

--- src/java/infra/services/product-api/application.tf
+++ src/java/infra/services/product-api/application.tf
@@ -279,5 +279,5 @@ resource "aws_ssm_parameter" "table_name_param" {
 resource "aws_ssm_parameter" "api_endpoint" {
   name  = "/java/tf/${var.env}/product/api-endpoint"
   type  = "String"
-  value = "${aws_api_gateway_stage.rest_api_stage.invoke_url}/${var.env}"
+  value = "${aws_api_gateway_stage.rest_api_stage.invoke_url}"
 }
