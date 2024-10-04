8f45fd1822c915205e91999ff986679842e652a9
james.eastham <james.eastham@datadoghq.com>
2024-10-04 13:58:42 +0100

    Updated topic naming

--- src/dotnet/infra/services/pricing-service/data.tf
+++ src/dotnet/infra/services/pricing-service/data.tf
@@ -9,7 +9,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_ssm_parameter" "product_created_topic_param" {
-  name = "/dotnet/tf/${var.env}//dotnet/product/product-created-topic"
+  name = "/dotnet/tf/${var.env}/product/product-created-topic"
 }
 
 data "aws_ssm_parameter" "product_updated_topic_param" {
