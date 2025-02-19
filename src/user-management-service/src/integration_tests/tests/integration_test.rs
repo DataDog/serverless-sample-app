bdb45aa204da0180c22216489a07cb23555b48bd
james.eastham <james.eastham@datadoghq.com>
2025-02-19 13:53:27 +0000

    Update Rust integration tests

--- src/user-management-service/src/integration_tests/tests/integration_test.rs
+++ src/user-management-service/src/integration_tests/tests/integration_test.rs
@@ -1,13 +1,10 @@
 use aws_config::BehaviorVersion;
-use driver::ApiDriver;
+use observability::TracedMessage;
+use reqwest::redirect::Policy;
+use serde_json::json;
 use serde::{Deserialize, Serialize};
-use std::result;
 use std::time::Duration;
 use tokio::time::sleep;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::{layer::SubscriberExt, Registry};
-
-mod driver;
 
 struct ApiEndpoint(String);
 struct EventBusName(String);
@@ -89,10 +86,11 @@ async fn when_user_registers_then_should_be_able_to_login() {
         .await
         .expect("Get user details response body should serialize to UserDTO");
 
-    println!("{}", user_response.data.email_address);
-    println!("{}", user_response.data.order_count);
-
     assert_eq!(user_response.data.order_count, 1);
+    assert_eq!(user_response.data.first_name, "Test");
+    assert_eq!(user_response.data.last_name, "Doe");
+    assert_eq!(user_response.data.email_address, email_under_test);
+    assert_eq!(user_response.data.user_id, email_under_test.to_uppercase());
 }
 
 async fn retrieve_paramater_values(environment: &str) -> (ApiEndpoint, EventBusName) {
@@ -125,4 +123,106 @@ async fn retrieve_paramater_values(environment: &str) -> (ApiEndpoint, EventBusN
         .expect("API Endpoint value not found");
 
     (ApiEndpoint(api_endpoint), EventBusName(event_bus_name))
+}
+
+pub(crate) struct ApiDriver {
+    env: String,
+    client: reqwest::Client,
+    eb_client: aws_sdk_eventbridge::Client,
+    base_url: String,
+    event_bus_name: String,
+}
+
+#[derive(Serialize)]
+pub struct UserCreatedEvent {
+    email_address: String,
+}
+
+impl ApiDriver {
+    pub async fn new(env: String, base_url: String, event_bus_name: String) -> Self {
+        let client = reqwest::Client::builder()
+            .redirect(Policy::none())
+            .build()
+            .expect("Failed to create reqwest client");
+
+        let config = aws_config::load_from_env().await;
+        let event_bridge_client = aws_sdk_eventbridge::Client::new(&config);
+
+        Self {
+            env,
+            client,
+            base_url,
+            event_bus_name,
+            eb_client: event_bridge_client,
+        }
+    }
+
+    pub async fn register_user(
+        &self,
+        email: &str,
+        first_name: &str,
+        last_name: &str,
+        password: &str,
+    ) -> reqwest::Response {
+        let register_body = json!({
+                "email_address": email,
+                "first_name": first_name,
+                "last_name": last_name,
+                "password": password
+            });
+
+        self.client
+            .post(&format!("{}/user", self.base_url))
+            .header("Content-Type", "application/json")
+            .body(register_body.to_string())
+            .send()
+            .await
+            .expect("Register user request failed")
+    }
+
+    pub async fn login_user(&self, email: &str, password: &str) -> reqwest::Response {
+        let login_body = json!({
+                "email_address": email,
+                "password": password
+            });
+
+        self.client
+            .post(&format!("{}/login", self.base_url))
+            .header("Content-Type", "application/json")
+            .body(login_body.to_string())
+            .send()
+            .await
+            .expect("Login user request failed")
+    }
+
+    pub async fn get_user_details(&self, email: &str, bearer_token: &str) -> reqwest::Response {
+        self.client
+            .get(&format!("{}/user/{}", self.base_url, email))
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {}", bearer_token))
+            .send()
+            .await
+            .expect("Get user details request failed")
+    }
+
+    pub async fn publish_order_completed_event(&self, email: &str) {
+        let payload = TracedMessage::new(UserCreatedEvent {
+            email_address: email.to_string(),
+        });
+        let payload_string = serde_json::to_string(&payload).expect("Error serde");
+
+        let request = aws_sdk_eventbridge::types::builders::PutEventsRequestEntryBuilder::default()
+            .set_source(Some(format!("{}.orders", &self.env)))
+            .set_detail_type(Some("orders.orderCompleted.v1".to_string()))
+            .set_detail(Some(String::from(payload_string)))
+            .set_event_bus_name(Some(self.event_bus_name.clone()))
+            .build();
+        let _ = self
+            .eb_client
+            .put_events()
+            .entries(request)
+            .send()
+            .await
+            .expect("Test event should publish");
+    }
 }
