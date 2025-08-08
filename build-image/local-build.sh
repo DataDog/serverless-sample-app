2921b3a4ee8f41268fddcdf71d1d826e2a3fea81
james.eastham <james.eastham@datadoghq.com>
2025-08-08 14:17:13 +0100

    feat: update build to set env vars

--- build-image/local-build.sh
+++ build-image/local-build.sh
@@ -4,19 +4,7 @@ set -ex
 echo "Pre-compile code to speed up first deployment."
 apt-get install git-all -y
 git clone https://github.com/DataDog/serverless-sample-app.git /serverless-sample-app
-cd /serverless-sample-app && ./build-all.sh
-
-# #############################################################
-# ### Install frontend and loadtesting dependencies         ###
-# #############################################################
-
-pushd /serverless-sample-app/src/frontend
-npm i && docker build .
-popd
-
-pushd /serverless-sample-app/loadtest
-npm i && docker build .
-popd
+cd /serverless-sample-app
 
 export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
@@ -48,6 +36,67 @@ export NVM_DIR="$HOME/.nvm"
 EOF
 
 source /root/.bashrc
+
+mkdir -p build-logs
+
+pushd src/shared-infra
+npm i
+popd
+
+pushd src/inventory-service
+echo "Building inventory service..."
+mvn clean package -DskipTests
+popd
+
+pushd src/user-management-service
+echo "Building user management service..."
+npm i
+./package.sh
+popd
+
+pushd src/loyalty-point-service
+echo "Building loyalty point service..."
+npm i
+./package.sh
+popd
+
+pushd src/pricing-service
+echo "Building pricing service..."
+npm i
+./package.sh
+popd
+
+pushd src/order-service
+echo "Building order service..."
+dotnet restore
+popd
+
+pushd src/order-service/src/Orders.BackgroundWorkers
+echo "Building order background workers..."
+dotnet lambda package
+popd
+
+pushd src/product-management-service
+echo "Building product management service..."
+make build
+popd
+
+pushd src/activity-service
+echo "Building activity service..."
+make dev && make deps && make build
+popd
+
+# #############################################################
+# ### Install frontend and loadtesting dependencies         ###
+# #############################################################
+
+pushd /serverless-sample-app/src/frontend
+npm i && docker build .
+popd
+
+pushd /serverless-sample-app/loadtest
+npm i && docker build .
+popd
 
 # Print app version information
 # node --version
