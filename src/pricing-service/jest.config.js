d2ac56e6022cd8175e3c337cc111f4d11ad409b9
james.eastham <james.eastham@datadoghq.com>
2026-03-11 17:53:50 +0000

    fix: exclude integration tests from default jest run in pricing service
    Integration tests require live AWS infrastructure (SSM, API endpoint)
    and should not run in CI without credentials. Added testPathIgnorePatterns
    to exclude tests/integration/ from the default npm test run.

--- src/pricing-service/jest.config.js
+++ src/pricing-service/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/integration/'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   }
