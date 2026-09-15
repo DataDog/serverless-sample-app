0f79601f1ce7bc3126e5cb7ed26a7c6a1993717e
James Eastham <james.eastham@datadoghq.com>
2026-09-15 13:45:15 +0100

    chore: bump loyalty stack versions

--- src/loyalty-point-service/lib/loyalty-api/loyaltyApiStack.ts
+++ src/loyalty-point-service/lib/loyalty-api/loyaltyApiStack.ts
@@ -30,8 +30,8 @@ export class LoyaltyApiStack extends cdk.Stack {
     });
 
     const datadogConfiguration = new DatadogLambda(this, "Datadog", {
-      nodeLayerVersion: 130,
-      extensionLayerVersion: 90,
+      nodeLayerVersion: 143,
+      extensionLayerVersion: 99,
       site: process.env.DD_SITE ?? "datadoghq.com",
       apiKeySecret: ddApiKey,
       service,
